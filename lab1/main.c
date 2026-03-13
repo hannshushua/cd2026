aa06c4b12c904f2ed0a6360854a29088e6930dd7
hannshushua <151006883+hannshushua@users.noreply.github.com>
2026-03-13 15:37:55 +0800

    finish main.c

---
+++ lab1/main.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node{
+    char c;
+    int numofchar;
+    struct node *next;
+} node;
+
+node *createnode(char c){
+    node *newnode = (node*)malloc(sizeof(node));
+    newnode->c = c;
+    newnode->numofchar = 1;
+    newnode->next = NULL;
+    return newnode;
+}
+node *findninsert(char c, node *head){
+    
+    if(head == NULL){
+        return createnode(c);
+    }else{
+        node *temp = head;
+        node *prev = NULL;
+        while(temp != NULL){
+            
+            // if(temp->c == c){
+            //     temp->numofchar++;
+            //     return head;
+            // }
+            prev = temp;
+            temp = temp->next;
+        }
+        
+        prev->next = createnode(c);
+        return head;
+    }
+}
+
+void printll(node *head) {
+    for (node *temp = head; temp != NULL ; temp = temp->next) {
+        char c = temp->c;
+        if(c == '\n'){
+            printf("\\n, ");
+        }else if(c == '\t') {
+            printf("\\t, ");
+        }else{
+            printf("%c, ", c);
+        }
+        
+    }
+}
+
+int main() {
+    FILE *file = fopen("main.c", "r"); 
+
+    if (file == NULL) {
+        perror("File opening failed");
+        return 1;
+    }
+    
+    node *head = NULL;
+    int ch;
+    while ((ch = fgetc(file)) != EOF) {
+        head = findninsert((char)ch, head);
+    }
+    
+    fclose(file);
+    
+    printll(head);
+    
+    node *current = head;
+    while (current != NULL) {
+        node *next = current->next;
+        free(current);
+        current = next;
+    }
+    
+    return 0;
+}
